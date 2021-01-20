c5188553dd3f3c8e362fb68bb5e389c9acda9e76
Ashraf-Sonde <ashrafsonde@gmail.com>
2021-01-20 12:35:59 +0530

    added the ham-overlay with animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
 
 btnHamburger.addEventListener('click', function() {
     console.log('hambuger menu');
 
-    if(btnHamburger.classList.contains('open')) {
-        btnHamburger.classList.remove('open');
-    } else {
-        btnHamburger.classList.add('open');
+    if(header.classList.contains('open')) { // close ham-menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else {    // open ham-menu
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
     }
     
 });
