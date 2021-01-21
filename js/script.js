5c1e455782d4fcd1f13c8231f144994c5e4c7cb1
Ashraf-Sonde <ashrafsonde@gmail.com>
2021-01-21 18:17:12 +0530

    done with the navigation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
 
 
 btnHamburger.addEventListener('click', function() {
+    body.classList.remove('noscroll');
     console.log('hambuger menu');
 
     if(header.classList.contains('open')) { // close ham-menu
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     } else {    // open ham-menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+        
     }
     
 });
